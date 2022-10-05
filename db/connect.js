82b7b0edc213e3049565faa60990acc2a4f3144c
Temitope Kolawole <kolabdul00@gmail.com>
2022-10-05 18:57:07 +0100

    initial commit

---
+++ db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+// const connectionString = process.env.MONOG_URI
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
